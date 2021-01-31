49ef12a50b97c1789b8066cbe7b5d6bbb96e2698
Merlin <Merlin@DESKTOP-R2SKS00>
2021-01-31 03:11:29 -0600

    Add .gitignore and .gitattributes.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Item
+{
+	public Item()    //Name       Discription         slot,str,agi,int,vit,wis,chr,lck
+	{
+		public string[] Stick = { "Stick", "A Sharpened Stick", "1", "2", "3", "0", "1", "0", "0", "0" }
+
+        public string[] ClothShirt = { "Cloth Shirt", "A basic cloth shirt", "5", "0", "0", "0", "1", "0","0", "0" }
+
+		
+
+	}
+}
